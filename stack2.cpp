34ba7df42fc969b7d1fae58c8d28c02bf50cf8dc
Koya Fukuda <kouyaf77@gmail.com>
2019-01-21 21:17:53 +0900

    Added queue, stack and vector as using stl

---
+++ stack2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+#include<stack>
+
+int main() {
+    stack<int> S;
+    int a, b, x;
+    string s;
+
+    while( cin >> s) {
+        if (s[0] == '+') {
+            a = S.top(); S.
+        }
+    }
+    return 0;
+}
