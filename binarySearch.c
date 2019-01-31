3c14afc93bc7b42ef34e51c9ec2f952b3f0ce0fe
Koya Fukuda <kouyaf77@gmail.com>
2019-02-01 07:36:53 +0900

    Added binarySearch

--- binarySearch.c
+++ binarySearch.c
@@ -10,7 +10,8 @@ int binarySearch(int key) {
     while (left < right)  {
         mid = (left + right) / 2;
         if (key == A[mid]) return 1;
-        else if (key > A[mid]) left = mid + 1;
+        
+        if (key > A[mid]) left = mid + 1;
         else if (key < A[mid]) right = mid;
     }
 
